83b9e3375e572c6333da4547efc80afda6a229c0
cbdyzj <cbdyzj@jianzhao.org>
2018-02-07 19:55:15 +0800

    update exceljs

--- code/exceljs.js
+++ code/exceljs.js
@@ -1,26 +1,20 @@
-const express = require('express')
 const { Workbook } = require('exceljs')
 
-const app = express()
-const workbook = new Workbook()
-const worksheet = workbook.addWorksheet('sheet')
+async function main() {
+    const workbook = new Workbook()
+    const worksheet = workbook.addWorksheet('sheet')
 
-worksheet.columns = [
-    { header: 'Id', key: 'id', width: 10 },
-    { header: 'Name', key: 'name', width: 30 },
-    { header: 'Age', key: 'age', width: 10 }
-]
+    worksheet.columns = [
+        { header: 'Id', key: 'id', width: 10 },
+        { header: 'Name', key: 'name', width: 30 },
+        { header: 'Age', key: 'age', width: 10 }
+    ]
+    worksheet.addRow({ id: 1, name: 'aa', age: 17 })
+    worksheet.eachRow(row => console.log(row.values))
 
-worksheet.addRow({ id: 1, name: 'aa', age: 17 })
-
-app.get('/excel', async (req, res) => {
-    res.setHeader('Content-Type', 'application/vnd.openxmlformats')
-    res.setHeader("Content-Disposition", "attachment;filename=" + encodeURI('workbook.xlsx'))
-    await workbook.xlsx.write(res)
-    res.end()
-})
+    // await workbook.xlsx.writeFile('a.xlsx')
+}
 
 if (require.main === module) {
-    workbook.xlsx.writeFile('excel.xlsx')
-    app.listen(3000)
+    main()
 }
