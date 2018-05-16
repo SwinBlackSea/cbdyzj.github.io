f625ed694c2132c22a099246e1bfc365ae106994
Cbdy <cbdyzj@jianzhao.org>
2018-05-16 22:36:56 +0800

    Update crawler.groovy

--- code/crawler.groovy
+++ code/crawler.groovy
@@ -1,7 +1,7 @@
 @Grab(group='org.jsoup', module='jsoup', version='1.10.1')
 import org.jsoup.Jsoup
 
-Jsoup.connect("https://v2ex.com").get()
-    .select("span.item_title > a").each {
-    println it.text() + ":" + it.attr("href")
+Jsoup.connect('https://v2ex.com').get()
+    .select('span.item_title > a').each {
+    println it.text() + ':' + 'https://v2ex.com' + it.attr('href')
 }
