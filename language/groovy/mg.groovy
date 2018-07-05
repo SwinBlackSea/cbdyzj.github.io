d922c63b9b185d20a0848270c3f0f389e211ff2a
cbdyzj <cbdyzj@jianzhao.org>
2018-07-05 22:19:16 +0800

    Add mysql

--- language/groovy/mg.groovy
+++ language/groovy/mg.groovy
@@ -1,5 +1,6 @@
 @Grab('org.mybatis.generator:mybatis-generator-core:1.3.6')
 @Grab('javax.xml.bind:jaxb-api:2.3.0')
+@Grab('mysql:mysql-connector-java:8.0.11')
 import org.mybatis.generator.api.MyBatisGenerator
 import org.mybatis.generator.config.xml.ConfigurationParser
 import org.mybatis.generator.internal.DefaultShellCallback
