81c5a15283d85fe293906ba97da953f5577ce867
cbdyzj <cbdyzj@jianzhao.org>
2018-09-05 19:55:53 +0800

    Amend

--- src/async_load.js
+++ src/async_load.js
@@ -3,35 +3,26 @@
  * http://jianzhao.org
  */
 
-class PageLoader {
-
-    constructor() {
-        this.container = document.querySelector('.markdown-body')
-    }
-
-    async load() {
-        const path = location.hash.substring(2) || '/blogs/目录'
-        return await this.loadPage(path)
-    }
-
-    async loadPage(path) {
+const navigator = {
+    getContainer: () => document.querySelector('.markdown-body'),
+    async navigate() {
+        const path = location.hash.substring(2) || '/index'
         const title = path.split('/').pop()
         document.title = title ? decodeURI(title) : '无可奉告'
 
         const response = await fetch(`${path}.md`)
         if (response.status !== 200) {
-            this.container.innerHTML = '<p>无可奉告<p>'
+            this.getContainer().innerHTML = '<p>无可奉告<p>'
             return
         }
         const content = await response.text()
-        this.container.innerHTML = marked(content)
+        this.getContainer().innerHTML = marked(content)
     }
 }
 
 document.onreadystatechange = function () {
     if (document.readyState === 'interactive') {
-        const loader = new PageLoader
-        addEventListener('popstate', () => loader.load())
-        loader.load()
+        addEventListener('popstate', () => navigator.navigate())
+        navigator.navigate()
     }
 }
