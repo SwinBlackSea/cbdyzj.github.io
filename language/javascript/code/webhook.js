b2b32eb5cb884dd14f3bfe90c390ebf3fd9b02d2
cbdyzj <cbdyzj@jianzhao.org>
2019-02-26 14:18:24 +0800

    Amend

--- language/javascript/code/webhook.js
+++ language/javascript/code/webhook.js
@@ -1,9 +1,9 @@
 const http = require('http')
 const { spawn } = require('child_process')
 
-function isMasterPush(event) {
-    return event.event_name === 'push'
-        && event.ref === 'refs/heads/master'
+function isMasterUpdate(event) {
+    return event.ref === 'refs/heads/master'
+        && ['push', 'merge'].includes(event.event_name)
 }
 
 function parseBody(req) {
@@ -18,13 +18,13 @@ function parseBody(req) {
 function runTask(...args) {
     const cp = spawn(...args)
     cp.stdout.on('data', (data) => console.log(`STDOUT:\n${data}`))
-    cp.stderr.on('data', (data) => console.error(`STDERR:\n${data}`))
+    cp.stderr.on('data', (data) => console.log(`STDERR:\n${data}`))
     cp.on('close', (code) => console.log(`EXIT CODE: ${code}`))
 }
 
 async function app(req, res) {
     const event = await parseBody(req)
-    if (isMasterPush(event)) {
+    if (isMasterUpdate(event)) {
         runTask('sh', ['./hook.sh'])
     }
     res.end()
